0540d402d7872c0d4e19fd1a53aebf76be8aaaaf
ahnwooseok <peterdntjr@naver.com>
2026-03-07 15:08:25 +0900

    push

--- class3/Main.java
+++ class3/Main.java
@@ -2,55 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static final int[] ry = {0, 0, 1, -1};
-    private static final int[] rx = {1, -1, 0, 0};
-    private static class Node{
-        int y, x, dist;
-        public Node (int y, int x, int dist){
-            this.y = y;
-            this.x = x;
-            this.dist = dist;
+    private static int N;
+    private static int max;
+    private static int[] array;
+    private static void dfs (int prev, int idx, int sum){
+        if (idx >= N) return;
+        else if (idx == N - 1){
+            sum += array[idx];
+            max = Math.max(max, sum);
+            return;
         }
+        else if (idx == -1){
+            dfs(1, 0, array[0]);
+            dfs(1, 1, array[1]);
+            dfs(1, 2, array[2]);
+        }
+        else{
+            if (prev == 2){
+                dfs(1, idx + 2, sum + array[idx]);
+            }
+            else{
+                dfs(2, idx + 1, sum + array[idx]);
+                dfs(1, idx + 2, sum + array[idx]);
+            }
+        }        
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[][] map = new int[N][M];
-        int[][] answer = new int[N][M];
-
-        ArrayDeque<Node> queue = new ArrayDeque<>();
-
-        for (int i = 0; i < N; i++){
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++){
-                int value = Integer.parseInt(st.nextToken());
-                if (value == 2){
-                    queue.offerLast(new Node(i, j, 0));
-                    answer[i][j] = 0;
-                }
-                else if (value == 1) answer[i][j] = -1;
-                map[i][j] = value;
-            }
-        }
-        
-        while (!queue.isEmpty()){
-            Node now = queue.pollFirst();
-            
-            for (int i = 0; i < 4; i++){
-                int dy = ry[i] + now.y;
-                int dx = rx[i] + now.x;
-                if (dy >= N || dy < 0 || dx >= M || dx < 0 || map[dy][dx] == 0 || answer[dy][dx] != -1) continue;
-                queue.offerLast(new Node(dy, dx, now.dist + 1));
-                answer[dy][dx] = now.dist + 1;
-            }
-        }
-
-        for (int i = 0; i < N; i++){
-            for (int j = 0; j < M; j++) System.out.print(answer[i][j] + " ");
-            System.out.println();
-        }
-
+        N = Integer.parseInt(br.readLine());
+        max = 0;
+        array = new int[N];
+        for (int i = 0; i < N; i++) array[i] = Integer.parseInt(br.readLine());
+        dfs(1, -1, 0);
+        System.out.println(max);
     }
 }
